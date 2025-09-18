57e322d48594c743173da65adca51589878e0292
Daniel Amado <daniel.amadove@gmail.com>
2025-09-18 12:05:15 -0500

    Implement theme toggle functionality: Add dark mode script to index.html, enhance Layout component with theme state management, and update utils for theme handling.

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,16 +1,16 @@
 import Sidebar from './Sidebar'
 import { Outlet } from 'react-router-dom'
-
 export default function Layout() {
+
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
+    <div className="min-h-screen bg-background text-foreground">
       <div className="md:flex md:min-h-screen">
         <div className="md:flex-shrink-0">
           <Sidebar />
         </div>
 
         <div className="flex-1">
-          <main className="dark:bg-gray-900  min-h-screen">
+          <main className="min-h-screen">
             <Outlet />
           </main>
         </div>
